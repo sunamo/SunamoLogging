c25b28d5daba81e1220451519ec8597d6135ab37
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-03-16 20:26:12 +0100

    .

--- _sunamo/SHJoinPairs.cs
+++ _sunamo/SHJoinPairs.cs
@@ -1,13 +1,26 @@
 namespace SunamoLogger._sunamo;
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace SunamoLogger._sunamo;
-//internal class SHJoinPairs
-//{
-//    internal static Func<string[], string> JoinPairs;
-//}
+internal class SHJoinPairs
+{
+    //internal static Func<string[], string> JoinPairs;
+
+    public static string JoinPairs(string firstDelimiter, string secondDelimiter, params string[] parts)
+    {
+        //InitApp.TemplateLogger.NotEvenNumberOfElements(type, "JoinPairs", @"args", parts);
+        //InitApp.TemplateLogger.AnyElementIsNull(type, "JoinPairs", @"args", parts);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < parts.Length; i++)
+        {
+            sb.Append(parts[i++] + firstDelimiter);
+            sb.Append(parts[i] + secondDelimiter);
+        }
+        return sb.ToString();
+    }
+}
