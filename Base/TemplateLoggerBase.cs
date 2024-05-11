46520358275623c49891950ba21b401fdb518397
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 13:28:20 +0200

    Removed usings outside of projects

--- Base/TemplateLoggerBase.cs
+++ Base/TemplateLoggerBase.cs
@@ -1,4 +1,4 @@
-using SunamoXlfKeys;
+
 
 namespace SunamoLogger.Base;
 
--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -4,15 +4,15 @@ global using System;
 global using System.Linq;
 global using System.Text;
 global using System.Collections;
-global using SunamoExceptions.OnlyInSE;
-global using SunamoValues;
+
+
 global using System.Diagnostics;
-global using SunamoExceptions;
+
 global using SunamoLogger.Base;
-global using SunamoInterfaces.Interfaces;
-global using SunamoExtensions;
-global using SunamoValues.Constants;
-global using SunamoTypeOfMessage;
+
+
+
+
 global using SunamoLogger.Logger.LoggerBaseNS;
 global using SunamoLogger._sunamo;
 
