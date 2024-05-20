9a839f18fde97bafd6a003baede34023f68abde1
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-20 06:51:20 +0200

    Before trying call automatic to fix remain errors

---
+++ _sunamo/SHSE.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunamoLogger;
+internal class SHSE
+{
+    internal static string NullToStringOrDefault(string text)
+    {
+        throw new NotImplementedException();
+    }
+}
