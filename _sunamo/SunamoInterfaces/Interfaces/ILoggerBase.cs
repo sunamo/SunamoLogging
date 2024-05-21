bd1b2734c29c436cfd3461bdd2744db25b48caf2
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-21 20:36:31 +0200

    Everyone on nuget

---
+++ _sunamo/SunamoInterfaces/Interfaces/ILoggerBase.cs
@@ -0,0 +1,19 @@
+namespace SunamoLogger;
+
+using System.Collections;
+
+public interface ILoggerBase
+{
+    void ClipboardOrDebug(string v, params string[] args);
+    bool IsInRightFormat(string text, params string[] args);
+    void TwoState(bool ret, params string[] toAppend);
+    void WriteCount(string collectionName, IList list);
+    void WriteLine(string what, object text);
+    void WriteLine(string text, params string[] args);
+    void WriteLineFormat(string v1, params string[] name);
+    void WriteLineNull(string text, params string[] args);
+    void WriteList(List<string> list);
+    void WriteList(string collectionName, List<string> list);
+    void WriteListOneRow(List<string> item, string swd);
+    void WriteNumberedList(string what, List<string> list, bool numbered);
+}
---
+++ _sunamo/SunamoValues/EmptyArrays.cs
@@ -0,0 +1,10 @@
+namespace SunamoLogger;
+
+
+public class EmptyArrays
+{
+    public static Char[] Chars = new Char[] { };
+    public static string[] Strings = new string[] { };
+    public static Object[] Objects = new Object[] { };
+    public static Byte[] Bytes = new Byte[] { };
+}
