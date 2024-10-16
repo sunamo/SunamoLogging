9fbf591a85f3b8104a8de6d5c1661c65497a14aa
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-16 12:28:29 +0200

    Removed unused file

--- _sunamo/SH.cs
+++ _sunamo/SH.cs
@@ -1,10 +1,4 @@
 namespace SunamoLogging._sunamo;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 internal class SH
 {
     internal static string NullToStringOrDefault(object n)
