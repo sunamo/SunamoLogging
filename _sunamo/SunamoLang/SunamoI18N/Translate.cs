3a43fe24dfaa9f09a8a4be2151f0f73e1317ae9b
sunamo.cz <radek.jancik@sunamo.cz>
2024-12-25 11:52:22 +0100

    Renamed file

--- _sunamo/SunamoLang/SunamoI18N/Translate.cs
+++ _sunamo/SunamoLang/SunamoI18N/Translate.cs
@@ -1,5 +1,5 @@
 namespace SunamoLogging._sunamo.SunamoLang.SunamoI18N;
-internal class sess
+internal class Translate
 {
     internal static string FromKey(string tryAFewSecondsLaterAfterFullyInitialized)
     {
