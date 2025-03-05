c5977d55113b8f8082e3d63e34ad2f054ef5ba69
sunamo.cz <radek.jancik@sunamo.cz>
2025-03-05 07:29:17 +0100

    Added global using of Microsoft.Extensions.Logging.Abstractions to all projects

--- FileLogger/ScopeDisposable.cs
+++ FileLogger/ScopeDisposable.cs
@@ -1,9 +1,4 @@
 namespace SunamoLogging.FileLogger;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 internal class ScopeDisposable : IDisposable
 {
